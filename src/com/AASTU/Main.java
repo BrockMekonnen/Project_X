f8d01b9c22231f2f2cdb175ebeffbb260ae7c02d
BrockMekonnen <BrockMekonnen@users.noreply.github.com>
2020-06-20 05:19:50 +0300

    Corrected the Structure of the Project

--- src/com/AASTU/Main.java
+++ src/com/AASTU/Main.java
@@ -1,6 +1,22 @@
 package com.AASTU;
 
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
+public class Main extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("View/IdentificationPane.fxml"));
+        AnchorPane pane = loader.load();
+        Scene scene = new Scene(pane);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
-
