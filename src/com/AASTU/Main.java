3837097b3baa5fdf0884726e9c09ff7d9a3915b7
biruk2000 <birukmolla30@gmail.com>
2020-06-20 07:53:23 +0300

    Login Page

--- src/com/AASTU/Main.java
+++ src/com/AASTU/Main.java
@@ -1,4 +1,32 @@
 package com.AASTU;
 
-public class Main {
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Main extends Application {
+
+//    Stage stage;
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+//        this.stage=primaryStage;
+//        mainWindow();
+        FXMLLoader loader = new FXMLLoader(Main.class.getResource("View/login.fxml"));
+        AnchorPane pane = loader.load();
+        Scene scene = new Scene(pane);
+        scene.getStylesheets().addAll(Main.class.getResource("View/LoginStyle.css").toExternalForm());
+        primaryStage.setTitle("login Page");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
