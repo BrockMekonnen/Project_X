1e9f3405836c33668a18e25561cb07ae59f255f9
BrockMekonnen <BrockMekonnen@users.noreply.github.com>
2020-06-21 10:23:51 +0300

    corrected SecretaryWindow and Login fxml files

--- src/com/AASTU/Controller/LoginController.java
+++ src/com/AASTU/Controller/LoginController.java
@@ -1,4 +1,13 @@
 package com.AASTU.Controller;
 
-public class LoginController {
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class LoginController implements Initializable{
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
 }
