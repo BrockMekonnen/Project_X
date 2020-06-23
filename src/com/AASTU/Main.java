9eebeb7609aeff282fe919cda2ce3a3d7bdb1163
Amanuel <amanab.449@gamil.com>
2020-06-23 14:44:05 +0300

    Adding Laboratory Window With animation

--- src/com/AASTU/Main.java
+++ src/com/AASTU/Main.java
@@ -1,6 +1,7 @@
 package com.AASTU;
 
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 
 import javafx.scene.Parent;
@@ -11,6 +12,12 @@ import javafx.stage.Stage;
 public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
+
+        FXMLLoader loader= new FXMLLoader(Main.class.getResource("View/DoctorWindow.fxml"));
+        Parent pane=loader.load();
+        Scene s=new Scene(pane);
+        primaryStage.setScene(s);
+        primaryStage.show();
 
     }
 
