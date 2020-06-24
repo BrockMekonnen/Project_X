5a175a5298deb4c5eda67ba838f9a5d1fc76173b
BrockMekonnen <BrockMekonnen@users.noreply.github.com>
2020-06-24 10:07:19 +0300

    added manager window and modified some other windows

--- src/com/AASTU/Main.java
+++ src/com/AASTU/Main.java
@@ -3,18 +3,18 @@ package com.AASTU;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 
+import javafx.scene.Group;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
+
 public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("View/login.fxml"));
-        Parent pane = loader.load();
-        Scene scene = new Scene(pane);
-        primaryStage.setScene(scene);
-        primaryStage.show();
+
+
     }
 
     public static void main(String[] args) {
