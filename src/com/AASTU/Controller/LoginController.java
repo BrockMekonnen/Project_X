cdca352d94d9b2cd9c66ec866df350edc7b369d2
biruk2000 <birukmolla30@gmail.com>
2020-06-29 14:46:39 +0300

    some connected Windows

--- src/com/AASTU/Controller/LoginController.java
+++ src/com/AASTU/Controller/LoginController.java
@@ -1,11 +1,31 @@
 package com.AASTU.Controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class LoginController implements Initializable{
+
+    @FXML
+    void ToIdentificationWindow(ActionEvent event) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("../View/IdentificationPane.fxml"));
+        Parent pane = loader.load();
+        Scene identificationScene =  new Scene(pane);
+        Stage identification = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        identification.setScene(identificationScene);
+        identification.show();
+
+    }
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
