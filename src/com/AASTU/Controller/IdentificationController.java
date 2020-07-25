47ba3fdd778d8132bd1d2b6822895f0afdfe5bf2
Amanuel <amanab.449@gamil.com>
2020-07-25 11:48:29 +0300

    Doctor Registration,Laboratory and Identification Pane modification

--- src/com/AASTU/Controller/IdentificationController.java
+++ src/com/AASTU/Controller/IdentificationController.java
@@ -73,52 +73,7 @@ public class IdentificationController implements Initializable{
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-//        FourInOnePane.setVisible(false);
-//        TransitionAnimation();
-//       Animation1();
-    }
 
-    public void TransitionAnimation(){
-        TranslateTransition translateTransition=new TranslateTransition(Duration.seconds(0.1),DoctorIdentificationBTN);
-        translateTransition.setByY(-800);
-        translateTransition.play();
-        TranslateTransition translateTransition1=new TranslateTransition(Duration.seconds(0.1),CardRoomIdentificationBTN);
-        translateTransition1.setByX(-800);
-        translateTransition1.play();
-        TranslateTransition translateTransition2=new TranslateTransition(Duration.seconds(0.1),LaboratoryIdentificaitonBTN);
-        translateTransition2.setByX(800);
-        translateTransition2.play();
-        TranslateTransition translateTransition3=new TranslateTransition(Duration.seconds(0.1),AdminIdentificationBTN);
-        translateTransition3.setByY(800);
-        translateTransition3.play();
-
-    }
-    public void Animation1(){
-        FourInOnePane.setVisible(true);
-        FadeTransition fadeTransition=new FadeTransition(Duration.seconds(1.5),FourInOnePane);
-        fadeTransition.setFromValue(0);
-        fadeTransition.setToValue(1);
-        fadeTransition.play();
-        fadeTransition.setOnFinished(event->{
-            TranslateTransition translateTransition=new TranslateTransition(Duration.seconds(0.4),DoctorIdentificationBTN);
-            translateTransition.setByY(800);
-            translateTransition.play();
-            translateTransition.setOnFinished(event1 -> {
-                TranslateTransition translateTransition1=new TranslateTransition(Duration.seconds(0.4),CardRoomIdentificationBTN);
-                translateTransition1.setByX(800);
-                translateTransition1.play();
-                translateTransition1.setOnFinished(event2 -> {
-                    TranslateTransition translateTransition2=new TranslateTransition(Duration.seconds(0.4),LaboratoryIdentificaitonBTN);
-                    translateTransition2.setByX(-800);
-                    translateTransition2.play();
-                    translateTransition2.setOnFinished(event3 -> {
-                        TranslateTransition translateTransition3=new TranslateTransition(Duration.seconds(0.4),AdminIdentificationBTN);
-                        translateTransition3.setByY(-800);
-                        translateTransition3.play();
-                    });
-                });
-            });
-        });
     }
 
 }
