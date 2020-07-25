fcc588f786dc96348f1535a40ca0bdb85f99c135
biruk2000 <birukmolla30@gmail.com>
2020-07-25 12:24:55 +0300

    signIn sample

--- src/com/AASTU/Controller/LoginController.java
+++ src/com/AASTU/Controller/LoginController.java
@@ -1,5 +1,6 @@
 package com.AASTU.Controller;
 
+import com.jfoenix.controls.JFXTextField;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -15,6 +16,47 @@ import java.util.ResourceBundle;
 
 public class LoginController implements Initializable{
 
+    public static String source;
+
+    @FXML
+    private JFXTextField userName;
+
+    @FXML
+    private JFXTextField password;
+
+
+    @FXML
+    void signIn(ActionEvent event) throws IOException {
+//        if(userName.getText().contains("doctor")) {
+//            changeWindow(event,"../view/DoctorWindow.fxml");
+//        }else if(userName.getText().contains("lab")){
+//            changeWindow(event,"../view/LaboratoryWindow.fxml");
+//        }else if(userName.getText().contains("sec")){
+//            changeWindow(event,"../view/SecretaryWindow.fxml");
+//        }else if(userName.getText().contains("manager")){
+//            changeWindow(event,"../view/ManagerWindow.fxml");
+//        }
+
+        if(source.equals("sec")){
+            changeWindow(event,"../view/SecretaryWindow.fxml");
+        } else if(source.equals("doc")){
+            changeWindow(event,"../view/DoctorWindow.fxml");
+        }else if(source.equals("mana")) {
+            changeWindow(event,"../view/ManagerWindow.fxml");
+        }else if(source.equals("lab")){
+            changeWindow(event,"../view/LaboratoryWindow.fxml");
+        }
+    }
+
+
+    public void changeWindow(ActionEvent event, String fxml) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource(fxml));
+        Parent pane = loader.load();
+        Scene loginScene =  new Scene(pane);
+        Stage loginwindow = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        loginwindow.setScene(loginScene);
+        loginwindow.show();
+    }
     @FXML
     void ToIdentificationWindow(ActionEvent event) throws IOException {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("../View/IdentificationPane.fxml"));
@@ -23,7 +65,6 @@ public class LoginController implements Initializable{
         Stage identification = (Stage) ((Node) event.getSource()).getScene().getWindow();
         identification.setScene(identificationScene);
         identification.show();
-
     }
 
     @Override
