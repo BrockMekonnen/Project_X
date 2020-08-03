c1c017ed35d4e180acd8ef8b006bd97d7b778b46
BrockMekonnen <BrockMekonnen@users.noreply.github.com>
2020-08-03 17:41:03 +0300

    mapped others class into the database

--- src/com/AASTU/Model/Others.java
+++ src/com/AASTU/Model/Others.java
@@ -1,10 +1,44 @@
 package com.AASTU.Model;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "others")
 public class Others {
+    @Id
+    @GeneratedValue
     private int labFormId;
+
+    @Embedded
+    @AttributeOverrides({
+            @AttributeOverride( name = "value", column = @Column(name = "gramStain_value")),
+            @AttributeOverride( name = "price", column = @Column(name = "garmStain_price")),
+            @AttributeOverride( name = "test", column = @Column(name = "gramStain_test"))
+    })
     private TestProperty gramStain;
+
+    @Embedded
+    @AttributeOverrides({
+            @AttributeOverride( name = "value", column = @Column(name = "wetFilm_value")),
+            @AttributeOverride( name = "price", column = @Column(name = "wetFilm_price")),
+            @AttributeOverride( name = "test", column = @Column(name = "wetFilm_test"))
+    })
     private TestProperty wetFilm;
+
+    @Embedded
+    @AttributeOverrides({
+            @AttributeOverride( name = "value", column = @Column(name = "afb_value")),
+            @AttributeOverride( name = "price", column = @Column(name = "afb_price")),
+            @AttributeOverride( name = "test", column = @Column(name = "afb_test"))
+    })
     private TestProperty afb;
+
+    @Embedded
+    @AttributeOverrides({
+            @AttributeOverride( name = "value", column = @Column(name = "hivAids_value")),
+            @AttributeOverride( name = "price", column = @Column(name = "hivAids_price")),
+            @AttributeOverride( name = "test", column = @Column(name = "hivAids_test"))
+    })
     private TestProperty hivAids;
 
     public Others(TestProperty gramStain, TestProperty wetFilm, TestProperty afb, TestProperty hivAids) {
