2e037370a169a2d9ad68e4a883c5193db1096d99
BrockMekonnen <BrockMekonnen@users.noreply.github.com>
2020-08-09 11:20:12 +0300

    added new window SecretaryPatientView

---
+++ src/com/AASTU/Controller/SecretaryPatientView.java
@@ -0,0 +1,13 @@
+package com.AASTU.Controller;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class SecretaryPatientView implements Initializable {
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
